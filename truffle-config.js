add72d64383a2e3aeb477c6b04380e3bce7c1b72
Jiranathanan Siripun <jiranathanan@gmail.com>
2024-12-15 15:35:53 +0700

    Setup Truffle, run truffle unbox instead of truffle init

---
+++ truffle-config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*", // Match any network id
+      gas: 5000000
+    },
+    contract_directory: './src/contracts',
+  },
+  compilers: {
+    solc: {
+      settings: {
+        optimizer: {
+          enabled: true, // Default: false
+          runs: 200      // Default: 200
+        },
+      }
+    }
+  }
+};
