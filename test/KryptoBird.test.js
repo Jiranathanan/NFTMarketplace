0368a935d27ee3ccc2627519667a13400eb447fe
Jiranathanan Siripun <jiranathanan@gmail.com>
2025-02-10 22:17:28 +0700

    Add assert to check an address

--- test/KryptoBird.test.js
+++ test/KryptoBird.test.js
@@ -14,8 +14,12 @@ contract('KryptoBird', (accounts) => {
     describe('deployment', async() => {
         // test samples with writing it
         it('deploys successfully', async () => {
-            contract = await KryptoBird.deployed()
-            
+            contract = await KryptoBird.deployed();
+            const address = contract.address; 
+            assert.notEqual(address, '')
+            assert.notEqual(address, null)
+            assert.notEqual(address, undefined)
+            assert.notEqual(address, 0x0)
         })
         
     });
