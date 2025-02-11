f533286576df956483f40295fcdce5163512846e
Jiranathanan Siripun <jiranathanan@gmail.com>
2025-02-11 11:37:00 +0700

    Write tests for name & symbol

--- test/KryptoBird.test.js
+++ test/KryptoBird.test.js
@@ -16,13 +16,26 @@ contract('KryptoBird', (accounts) => {
         it('deploys successfully', async () => {
             contract = await KryptoBird.deployed();
             const address = contract.address; 
-            assert.notEqual(address, '')
-            assert.notEqual(address, null)
-            assert.notEqual(address, undefined)
-            assert.notEqual(address, 0x0)
+            assert.notEqual(address, '');
+            assert.notEqual(address, null);
+            assert.notEqual(address, undefined);
+            assert.notEqual(address, 0x0);
+        })
+        // 1. Test that the name matches on our contract using the assert.equal function
+        // 2. Test that the symbol matches with the assert.equal
+        it('has a name', async() => {
+            const name = contract.name();
+            assert.equal(name, 'KryptoBird');
+        })
+        it('has a symbol', async() => {
+            const symbol = contract.symbol();
+            assert.equal(symbol, 'KBIRDZ');
         })
         
     });
+
+       
+
     
 
 })
